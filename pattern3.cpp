3113ff95b0943e8c5abbfa6567fdcac07bd0f47d
shilpykrisna <113418765+shilpykrisna@users.noreply.github.com>
2022-09-27 07:58:34 +0530

    Update pattern3.cpp

--- pattern3.cpp
+++ pattern3.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            char ch='A'+i-1;  // this is important otherwise it has typecasted into numerical form
+            cout<<ch;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+}
