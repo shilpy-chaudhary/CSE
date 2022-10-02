d1027d7dfe3116cfb3a168bcacdca293b3ff9983
shilpykrisna <113418765+shilpykrisna@users.noreply.github.com>
2022-10-02 12:34:40 +0530

    Update complement of base interger 10.cpp

--- complement of base interger 10.cpp
+++ complement of base interger 10.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+        int m=n;
+        int mask =0;
+
+        while(m!=0){
+            mask = (mask<<1) | 1;
+            m = m>>1;
+        }
+        int ans=(~n) & mask;
+        return ans;
+    }
+};
 
