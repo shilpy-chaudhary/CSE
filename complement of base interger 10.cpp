d23183d38617adcce9036e11174f97060461011f
shilpykrisna <113418765+shilpykrisna@users.noreply.github.com>
2022-10-02 12:37:29 +0530

    Update complement of base interger 10.cpp

--- complement of base interger 10.cpp
+++ complement of base interger 10.cpp
@@ -3,6 +3,9 @@ using namespace std;
 int main(){
         int m=n;
         int mask =0;
+        if(n==0){
+                return 0;
+        }
 
         while(m!=0){
             mask = (mask<<1) | 1;
