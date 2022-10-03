d4612bb3f01bd3648092d95993164a51e02c3de2
shilpykrisna <113418765+shilpykrisna@users.noreply.github.com>
2022-10-03 16:12:08 +0530

    Update min and max of array.cpp

--- min and max of array.cpp
+++ min and max of array.cpp
@@ -1,1 +1,37 @@
+#include<iostream>
+using namespace std;
 
+int getmax(int num[],int n){
+    int maxi =INT_MIN;
+
+    for(int i=0; i<n;i++){
+        maxi= max(maxi, num[i]);
+
+    }
+    return maxi;
+}
+
+int getmin(int num[],int n){
+    int mini = INT_MAX;
+
+    for(int i=0;i<n;i++){
+        mini=min(mini,num[i]);
+
+    }
+    return mini;
+}
+
+
+int main(){
+     int size;
+     cin>>size;
+     int num[100];
+
+     for(int i=0; i<size; i++){
+        cin>>num[i];
+
+     }
+     cout<<"min number is"<<getmin(num,size)<<endl;
+     cout<<"max number is "<<getmax(num,size)<<endl;
+
+}
