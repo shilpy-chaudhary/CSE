ee8c92ed80012262146a68fc5949908c677894dc
shilpykrisna <113418765+shilpykrisna@users.noreply.github.com>
2022-10-04 12:13:13 +0530

    Update sum of even numbers.cpp

--- sum of even numbers.cpp
+++ sum of even numbers.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    int n;
+    cin>>n;
+    int i =0;
+    int sum=0;
+    while(i<=n){
+        if(i%2==0){
+            sum =sum+i;
+        
+        }
+        i++;
+    }
+    cout<<sum<<endl;
+    
+    }
